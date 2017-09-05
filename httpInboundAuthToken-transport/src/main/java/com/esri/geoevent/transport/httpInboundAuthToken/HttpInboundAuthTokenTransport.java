93cddba188c89bdc53e34db5e6161d243efda6fe
Hanoch Kalmanovich <hkalmanovich@esri.com>
2017-09-05 10:19:05 -0700

    updated licensing

--- httpInboundAuthToken-transport/src/main/java/com/esri/geoevent/transport/httpInboundAuthToken/HttpInboundAuthTokenTransport.java
+++ httpInboundAuthToken-transport/src/main/java/com/esri/geoevent/transport/httpInboundAuthToken/HttpInboundAuthTokenTransport.java
@@ -1,5 +1,5 @@
 /*
-  Copyright 1995-2014 Esri
+  Copyright 2017 Esri
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -11,7 +11,7 @@
     distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
-    limitations under the License.
+    limitations under the License.​
 
   For additional information, contact:
   Environmental Systems Research Institute, Inc.
@@ -20,7 +20,7 @@
   Redlands, California, USA 92373
 
   email: contracts@esri.com
- */
+*/
 
 package com.esri.geoevent.transport.httpInboundAuthToken;
 
